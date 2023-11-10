4e6e5b3f78076e8a243cf014dfb7030e9d56ba90
nikii-me <ikstrem78@gmail.com>
2023-11-10 18:35:45 +0100

    Added plenty of virtual functions from vftable, more to come

--- Spore ModAPI/Spore/Simulator/cMissionChangeArchetype.h
+++ Spore ModAPI/Spore/Simulator/cMissionChangeArchetype.h
@@ -8,7 +8,25 @@ namespace Simulator
 	{
 	public:
 		static const uint32_t TYPE = 0x462227e;
-		static const uint32_t NOUN_ID = 0x4622285;
+		static const uint32_t NOUN_ID = 0x4622285;	
+		/* 8h */	void func8h();									
+		/* 10h */	virtual bool Write(ISerializerStream* stream) override;
+		/* 14h */	virtual bool Read(ISerializerStream* stream) override;
+		/* 16h */	virtual bool WriteToXML(XmlSerializer*) override;
+		/* 20h */	virtual uint32_t GetNounID() const override;
+		/* 38h */	virtual uint32_t GetCastID() const override;
+
+		/* 74h */	virtual uint32_t GetCardWindowControlID();
+		/* 78h */	virtual void Initialize() override;
+		/* 7Ch */	virtual MissionState Update(int deltaTime) override;
+		/* 80h */	virtual void Load() override;
+		/* 84h */	virtual void ShutdownSystems() override;
+		/* 88h */	virtual void OnMissionAccept() override;
+		/* 8Ch */	virtual void OnMissionReject() override;
+		/* 90h */	virtual void OnMissionAbort() override;
+		/* 94h */	virtual void OnMissionCompleted();
+		/* 98h */	virtual void OnMissionFailed();
+		/* 9Ch */	virtual void FailMission();
 	public:
 		/* 1F0h */	int mChangeArchetypeState;
 		/* 1F4h */	Simulator::Archetypes mChangeArchetypeTo;
