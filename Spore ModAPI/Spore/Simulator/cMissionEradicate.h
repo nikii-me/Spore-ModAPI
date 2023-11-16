e6e5d267c9f1d43c78d5904b52ddde702da673be
nikii-me <ikstrem78@gmail.com>
2023-11-16 22:50:59 +0100

    Finished tidying up already existing cMission subclasses, working again on finding new ones.

--- Spore ModAPI/Spore/Simulator/cMissionEradicate.h
+++ Spore ModAPI/Spore/Simulator/cMissionEradicate.h
@@ -3,13 +3,13 @@
 
 namespace Simulator
 {
+	/// Represents the eradication missions in space stage.
 	class cMissionEradicate
-		: cMission
+		: public cMission
 	{
 	public:
 		static const uint32_t TYPE = 0x2f9998c;
 		static const uint32_t NOUN_ID = 0x2f99994;
-
 
 		/* 10h */	virtual bool Write(ISerializerStream* stream) override;
 		/* 14h */	virtual bool Read(ISerializerStream* stream) override;
@@ -20,7 +20,7 @@ namespace Simulator
 		/// Returns mNumDiseasedKills.
 		/* 54h */	virtual int func54h();
 
-		/// Returns mTotalNumKillsNeeded
+		/// Returns mTotalNumKillsNeeded.
 		/* 58h */	virtual int func58h();
 
 		/// Shares the address with func58h().
@@ -42,23 +42,42 @@ namespace Simulator
 		/* 118h */	virtual bool TranslateToken(uint32_t tokenID, eastl::string16& dst) override;
 		/* 11Ch */	virtual uint32_t GetCompletedEventLogID() override;
 		/* 124h */	virtual void func120h(int) override;
-		/* 140h */	virtual bool func140h(int, int) override;			//Checks if mEradicateType == CityCitizens (0x1e8763), then returns true if 1st param equals mpTargetPlanet(?)
+
+		/// Checks if mEradicateType is equal to 0x1e8763 (CityCitizens) and returns true
+		/// if the first parameter is equal to mpTargetPlanet.
+		/* 140h */	virtual bool func140h(int, int) override;
+
 		/* 144h */	virtual void AddMessageListeners() override;
 		/* 148h */	virtual void RemoveMessageListeners() override;
-		/* 15Ch */	virtual bool func15Ch() override;					//Passes class' this to another function
-		/* 184h */	virtual float func184h(int, int) override;
-		/* 188h */	virtual int CalculateMissionDuration() override;	//Assigns mTotalNumKillsNeeded to mDuration
-		/* 18Ch */	virtual bool PickTargetAnimalSpecies() override;	//First check is mEradicateType != Animals
 
-		/* 1A4h */	virtual int32_t func1A4h(int, int, int);				//Returns value of property missionEradicateMaxNumHealthyKills (0xbeac6fe1)
-		/* 1A8h */	virtual int32_t func1A8h(int, int, int);				//Returns value of property missionEradicateNumKillsNeeded (0xbeac6fe2)
-		/* 1ACh */	virtual int32_t func1ACh(int, int, int);				//Returns value of property missionEradicateNumCreatures (0xc8d23e7f)
-		/* 1B0h */	virtual int func1B0h();								//mMaxNumDiseasedCreatures - mNumDiseasedKills
-		/* 1B4h */	virtual int32_t func1B4h(int, int, int);				//Returns value of property missionEradicateMinPerHerd (0x7a2db2ee)
-		/* 1B8h */	virtual int32_t func1B8h(int, int, int);				//Returns value of property missionEradicateMaxPerHerd (0x7a2db2ef)
+		/* 15Ch */	virtual bool func15Ch() override;
+		/* 184h */	virtual float func184h(int, int) override;
+
+		/// Assigns the value of mTotalNumKillsNeeded to mDuration.
+		/* 188h */	virtual int CalculateMissionDuration() override;
+
+		/* 18Ch */	virtual bool PickTargetAnimalSpecies() override;
+
+		/// Returns the value of property missionEradicateMaxNumHealthyKills (0xbeac6fe1).
+		/* 1A4h */	virtual int func1A4h(int, int, int);				//Returns value of property missionEradicateMaxNumHealthyKills (0xbeac6fe1)
+
+		/// Returns the value of property missionEradicateNumKillsNeeded (0xbeac6fe2).
+		/* 1A8h */	virtual int func1A8h(int, int, int);
+
+		/// Returns the value of property missionEradicateNumCreatures (0xc8d23e7f).
+		/* 1ACh */	virtual int func1ACh(int, int, int);
+
+		/// Returns the value of mMaxNumDiseasedCreatures - mNumDiseasedKills.
+		/* 1B0h */	virtual int func1B0h();
+
+		/// Returns the value of property missionEradicateMinPerHerd (0x7a2db2ee).
+		/* 1B4h */	virtual int func1B4h(int, int, int);
+
+		/// Returns the value of property missionEradicateMaxPerHerd (0x7a2db2ef).
+		/* 1B8h */	virtual int func1B8h(int, int, int);				
 
 	public:
-		/* 1F0h */	int mEradicateType;				//-1
+		/* 1F0h */	int mEradicateType;				// -1
 		/* 1F4h */	int mEradicateMissionState;
 		/* 1F8h */	int mMaxNumDiseasedCreatures;
 		/* 1FCh */	int mMinDiseasedPerHerd;
@@ -67,10 +86,10 @@ namespace Simulator
 		/* 208h */	int mNumDiseasedKills;
 		/* 20Ch */	int mMaxNumHealthyKills;
 		/* 210h */	int mNumHealthyKills;
-		/* 214h */	uint8_t mCanFail;				//1
-		/* 218h */	int field_218;					//-1
-		/* 21Ch */	int field_21C;					//-1
-		/* 220h */	int field_220;					//eastl allocator called
+		/* 214h */	uint8_t mCanFail;				// 1
+		/* 218h */	int field_218;					// -1
+		/* 21Ch */	int field_21C;					// -1
+		/* 220h */	int field_220;					// eastl allocator called
 		/* 224h */	int field_224;
 		/* 228h */	int field_228;
 	};
--- Spore ModAPI/Spore/Simulator/cMissionExplore.h
+++ Spore ModAPI/Spore/Simulator/cMissionExplore.h
@@ -3,8 +3,9 @@
 
 namespace Simulator
 {
+	/// Represents the first tutorial mission, which is skippable.
 	class cMissionExplore
-		: cMission
+		: public cMission
 	{
 	public:
 		static const uint32_t TYPE = 0x32a12f0;
@@ -15,9 +16,16 @@ namespace Simulator
 		/* 1Ch */	virtual bool WriteToXML(XmlSerializer*) override;
 		/* 20h */	virtual uint32_t GetNounID() const override;
 		/* 38h */	virtual uint32_t GetCastID() const override;
-		/* 54h */	virtual int func54h(IStream*);							//Checks if mVisitCities != null, then returns mNumVisitedCities		
-		/* 58h */	virtual int func58h(IStream*);							//Same as func54h(), except returns mNumCitiesToVisit		
-		/* 5Ch */	virtual int func5Ch(IStream*);							//Same as func58h()
+
+		/// Returns mNumVisitedCities if mVisitCities isn't null.
+		/* 54h */	virtual int func54h(IStream*);
+
+		/// Returns mNumCitiesToVisit if mVisitCities isn't null.
+		/* 58h */	virtual int func58h(IStream*);
+
+		/// Shares the address with func58h().
+		/* 5Ch */	virtual int func5Ch(IStream*);
+
 		/* 74h */	virtual uint32_t GetCardWindowControlID() override;
 		/* 78h */	virtual void Initialize() override;
 		/* 7Ch */	virtual MissionState Update(int deltaTime) override;
@@ -27,13 +35,26 @@ namespace Simulator
 		/// WARNING: This function shares the same address with other cMission subclasses!
 		/// Use virtual_detour with this function to get an appropriate 'this' parameter.
 		/// You might also want to check from which class this function is called as well.
-		/* 94h */	virtual void OnMissionCompleted() override;
-
+		/// The address is shared with cMissionBalance, cMissionChangeArchetype, ....
 		/* B8h */	virtual void GetTitleText(eastl::string16& dst) override;
+
+		/// WARNING: This function shares the same address with other cMission subclasses!
+		/// Use virtual_detour with this function to get an appropriate 'this' parameter.
+		/// You might also want to check from which class this function is called as well.
+		/// The address is shared with cMissionChangeArchetype, ....
 		/* F8h */	virtual bool HasBeenFulfilled() override;
+
 		/* 118h */	virtual bool TranslateToken(uint32_t tokenID, eastl::string16& dst) override;
-		/* 120h */	virtual bool func120h() override;								//Returns mVisitCites
+
+		/// Returns mVisitCities.
+		/* 120h */	virtual bool func120h() override;
+
+		/// WARNING: This function shares the same address with other cMission subclasses!
+		/// Use virtual_detour with this function to get an appropriate 'this' parameter.
+		/// You might also want to check from which class this function is called as well.
+		/// The address is shared with cMissionEradicate.
 		/* 124h */	virtual void func120h(int) override;
+
 		/* 144h */	virtual void AddMessageListeners() override;
 		/* 148h */	virtual void RemoveMessageListeners() override;
 
@@ -44,21 +65,22 @@ namespace Simulator
 
 		/// Functionality-wise this function behaves the same as cMission::func178h(), but it
 		/// also includes a null check for mVisitCities.
-		/* 178h */  virtual uint32_t func178h() override;							//Same thing as cMission::func178h(), but also checks for if mVisitCities != null
+		/* 178h */  virtual uint32_t func178h() override;
+
 		/* 17Ch */	virtual uint32_t GetCreatureGoalCardLayoutId() override;
 
-		/// Null check for mVisitCities
+		/// Checks if mVisitCities isn't null.
 		/* 180h */	virtual bool func180h() override;
 
 	public:
 		/* 1F0h */	int mExploreMissionState;
-		/* 1F4h */	uint8_t mVisitCities;				// A class pointer perhaps?
+		/* 1F4h */	uint8_t mVisitCities;				// A class pointer maybe?
 		/* 1F8h */	int mVisitRadius;
 		/* 1FCh */	int mEffectHeight;
 		/* 200h */	int field_200;
 		/* 204h */	int mNumCitiesToVisit;
 		/* 208h */	int mNumVisitedCities;
-		/* 20Ch */	int mExploreType;					// Referenced in Write() function, not found in the constructor??
+		/* 20Ch */	int mExploreType;					// Referenced in Write() function, not found in the constructor
 		/* 210h */	bool mCityVisitInfosInitialized;
 		/* 214h */	int mCityVisitInfos;				// Array of ints? Attribute has a custom Read and Write function
 		/* 218h */	int field_218;
